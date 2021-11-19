37226aacac31578aea9bd697aead38ae4348766a
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2021-11-19 15:18:43 -0300

    Melhorando nosso componente

--- src/components/basics/Primeiro.js
+++ src/components/basics/Primeiro.js
@@ -1,3 +1,11 @@
+import React from "react";
+
 export default function Primeiro() {
-    return 'Primeiro Componente'
+    const msg = "Seja Bem-vindo(a)!"
+    return (
+        <div>
+            <h2>Primeiro Componente</h2>
+            <p>{msg}</p>
+        </div>
+    )
 }
