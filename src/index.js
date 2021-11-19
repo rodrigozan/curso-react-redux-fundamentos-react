be25fdb32749755303e5853666dc16db587a6541
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2021-11-19 15:10:02 -0300

    Introdução ao JSX

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
 
-const el = document.getElementById('root')
-ReactDOM.render("Olá React!!!", el)
+const tag = <p>Olá <strong>React</strong>!!!</p>
+
+ReactDOM.render(
+    <div>
+        { tag }
+    </div>, 
+    document.getElementById('root')
+)
